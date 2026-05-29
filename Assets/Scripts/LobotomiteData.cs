fcea8ad3bb8b3dc2403918f11759c60ca01818d9
Serhiiko <serikgorbanev@gmail.com>
2026-05-29 18:08:21 +0200

    Added visuals to lobotomite data

--- Assets/Scripts/LobotomiteData.cs
+++ Assets/Scripts/LobotomiteData.cs
@@ -9,4 +9,11 @@ public class LobotomiteData : ScriptableObject
 	[SerializeField] public int _Prudence;
 	[SerializeField] public int _Temperance;
 	[SerializeField] public int _Justice;
+	
+	[SerializeField] public int _MouthIdx;
+	[SerializeField] public int _EyesIdx;
+	
+	[SerializeField] public int _HairFrontIdx;
+	[SerializeField] public int _HairBackIdx;
+	[SerializeField] public Color _HairColor;
 }
